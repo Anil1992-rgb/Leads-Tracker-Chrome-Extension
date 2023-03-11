37481869fc1c0db844ba83ebdeb969294189f2be
Anil Canbolat <alancturk1992@gmail.com>
2023-03-10 17:17:35 -0800

    added json file, styling, and JS code

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ const inputEl = document.getElementById("input-el")
 const inputBtn = document.getElementById("input-btn")
 const ulEl = document.getElementById("ul-el")
 
+localStorage.setItem("myLeads", "www.wdwd.com")
+
 inputBtn.addEventListener("click", function () {
     myLeads.push(inputEl.value)
     inputEl.value = ""
@@ -12,8 +14,12 @@ inputBtn.addEventListener("click", function () {
 function renderLeads() {
     let listItems = ""
     for (let i = 0; i < myLeads.length; i++) {
-        listItems += "<li>" + myLeads[i] + "</li>"
+        listItems += `
+        <li>
+            <a target='_blank' = href='${myLeads[i]}'>
+                ${myLeads[i]}
+            </a>
+        </li>`
     }
     ulEl.innerHTML = listItems
 }
-
